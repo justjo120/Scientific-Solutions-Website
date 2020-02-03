1093c184817525fbd6a538e34ec6309363ad99e0
justjo120 <johannarklay@gmail.com>
2020-02-03 13:03:02 -0600

    Add About Content

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,45 @@
 <template>
-  <v-layout>
-    <v-flex>Here at Scientific Solutions, we bring experienced, innovative solutions consistently exceed our customers expectations. If you’re ready for a superior chemical purchasing experience, request a quote today.</v-flex>
-  </v-layout>
+  <v-container>
+    <v-layout class="text-center">
+      <v-flex>
+        <p />
+        <div class="display-1">About Us</div>
+        <v-img
+          src="https://i.ibb.co/7p0SHP9/alga-algae-analysis-background-2280548.jpg"
+          aspect-ratio="1.5"
+          contain
+        ></v-img>
+        <p />
+        Here at Scientific Solutions, we bring experienced, innovative solutions
+        consistently exceed our customers expectations. If you’re ready for a
+        superior chemical purchasing experience, request a quote today.
+        <v-flex>
+          <p />
+          <div class="title">Charities We Contribute To:</div>
+          <p />
+          <v-row>
+            <v-img
+              src="https://i.ibb.co/kKstNtV/Feeding-America-logo1.jpg"
+              alt="Feeding-America-logo1"
+              aspect-ratio="1.5"
+              contain
+            ></v-img>
+            <v-img
+              src="https://i.ibb.co/wpdFmrv/St-Jude-emblem.jpg"
+              alt="St-Jude-emblem"
+              aspect-ratio="1.5"
+              contain
+            ></v-img></v-row
+        ></v-flex>
+        <div class="title">Meet Us At a Future Conference!</div>
+        <p />
+        <v-img
+          src="https://i.ibb.co/phqwbw2/44262558-1894966373891655-823415689581690880-o.jpg"
+          alt="44262558-1894966373891655-823415689581690880-o"
+          aspect-ratio="1.5"
+          contain
+        ></v-img>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
